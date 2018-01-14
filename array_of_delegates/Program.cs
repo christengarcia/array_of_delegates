86d16678a28071dcf264eb880239cb469fd05d25
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-14 16:32:03 +1100

    Add Main

--- array_of_delegates/Program.cs
+++ array_of_delegates/Program.cs
@@ -23,7 +23,26 @@ namespace array_of_delegates
         {
             return Radius * Radius * PI;
         }
+    }
 
-
+    public static class Program
+    {
+        static int Main()
+        {
+            double R = 10;
+            Circle circ = new Circle();
+            Measure[] Calc = new Measure[3];
+            Calc[0] = new Measure(circ.Diameter);
+            double D = Calc[0](R);
+            Calc[1] = new Measure(circ.Circumference);
+            double C = Calc[1](R);
+            Calc[2] = new Measure(circ.Area);
+            double A = Calc[2](R);
+            Console.WriteLine("Diameter:      {0}", D);
+            Console.WriteLine("Circumference: {0}", C);
+            Console.WriteLine("Area:          {0}", A);
+            Console.ReadLine();
+            return 0;
+        }
     }
 }
