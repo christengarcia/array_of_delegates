01a369425f85595c6620fc682c9a277ab7e54a2a
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-14 16:22:48 +1100

    Initial commit

---
+++ array_of_delegates/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * C# Program to Illustrate Array of Delegates
+ */
+
+using System;
+delegate double Measure(double R);
+namespace array_of_delegates
+{
+    class Circle
+    {
+        const double PI = 3.14159;
+        public double Diameter(double Radius)
+        {
+            return Radius * 2;
+        }
+
+
+    }
+}
